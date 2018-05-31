91e7f0b2e13045fc123488015d910eeadbcb5fba
informatica4 <informatica4@RafaelGomez>
2018-05-31 13:05:58 +0200

    Versión 1 Rama 2

--- src/pruebanetbeans/PruebaNetBeans.java
+++ src/pruebanetbeans/PruebaNetBeans.java
@@ -17,6 +17,7 @@ public class PruebaNetBeans {
     public static void main(String[] args) {
         // TODO code application logic here
         //Nada de nada
+        //Desarrollo rama2
     }
     
 }
